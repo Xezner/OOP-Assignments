9dc7140e6dce6757762b20b8f47b47d9a2133b64
Xezner <prencylabueg@gmail.com>
2021-11-26 23:53:57 +0800

    Finished making contract tracing form

---
+++ Abueg_Assignment_06/Abueg_Assignment_06/Form1.Designer.cs
@@ -0,0 +1,319 @@
+﻿
+namespace Abueg_Assignment_06
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.logo = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bodyTemp = new System.Windows.Forms.Label();
+            this.dateTime = new System.Windows.Forms.Label();
+            this.name = new System.Windows.Forms.Label();
+            this.address = new System.Windows.Forms.Label();
+            this.number = new System.Windows.Forms.Label();
+            this.email = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.tempBox = new System.Windows.Forms.TextBox();
+            this.dateBox = new System.Windows.Forms.TextBox();
+            this.nameBox = new System.Windows.Forms.TextBox();
+            this.addressBox = new System.Windows.Forms.TextBox();
+            this.numberBox = new System.Windows.Forms.TextBox();
+            this.emailBox = new System.Windows.Forms.TextBox();
+            this.submit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.logo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // logo
+            // 
+            this.logo.Image = ((System.Drawing.Image)(resources.GetObject("logo.Image")));
+            this.logo.InitialImage = ((System.Drawing.Image)(resources.GetObject("logo.InitialImage")));
+            this.logo.Location = new System.Drawing.Point(347, 3);
+            this.logo.Name = "logo";
+            this.logo.Size = new System.Drawing.Size(421, 75);
+            this.logo.TabIndex = 0;
+            this.logo.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(277, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(547, 44);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Visitor Contract Tracing Form";
+            // 
+            // bodyTemp
+            // 
+            this.bodyTemp.AutoSize = true;
+            this.bodyTemp.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bodyTemp.Location = new System.Drawing.Point(109, 172);
+            this.bodyTemp.Name = "bodyTemp";
+            this.bodyTemp.Size = new System.Drawing.Size(139, 26);
+            this.bodyTemp.TabIndex = 2;
+            this.bodyTemp.Text = "Body Temp:";
+            // 
+            // dateTime
+            // 
+            this.dateTime.AutoSize = true;
+            this.dateTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTime.Location = new System.Drawing.Point(109, 205);
+            this.dateTime.Name = "dateTime";
+            this.dateTime.Size = new System.Drawing.Size(174, 26);
+            this.dateTime.TabIndex = 3;
+            this.dateTime.Text = "Date and Time:";
+            // 
+            // name
+            // 
+            this.name.AutoSize = true;
+            this.name.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.name.Location = new System.Drawing.Point(109, 239);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(82, 26);
+            this.name.TabIndex = 4;
+            this.name.Text = "Name:";
+            // 
+            // address
+            // 
+            this.address.AutoSize = true;
+            this.address.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.address.Location = new System.Drawing.Point(109, 272);
+            this.address.Name = "address";
+            this.address.Size = new System.Drawing.Size(99, 26);
+            this.address.TabIndex = 5;
+            this.address.Text = "Address";
+            // 
+            // number
+            // 
+            this.number.AutoSize = true;
+            this.number.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.number.Location = new System.Drawing.Point(109, 306);
+            this.number.Name = "number";
+            this.number.Size = new System.Drawing.Size(145, 26);
+            this.number.TabIndex = 6;
+            this.number.Text = "Contact No.:";
+            // 
+            // email
+            // 
+            this.email.AutoSize = true;
+            this.email.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.email.Location = new System.Drawing.Point(109, 341);
+            this.email.Name = "email";
+            this.email.Size = new System.Drawing.Size(80, 26);
+            this.email.TabIndex = 7;
+            this.email.Text = "Email:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(120, 424);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(917, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "I hereby authorize Harbor POint Mall to collect and process the data herein for t" +
+    "he purpose of the contact tracing effecting control";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(116, 444);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(923, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "of the COVID19 transmission. I understand my personal information is protected by" +
+    " RA 10173 or the Data Privacy Act of 2012 and";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(98, 464);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(968, 20);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "that this form will be destoryed after 30 days from the data of accomplishment, f" +
+    "ollowing the National Archives of the Phillippines protocol.";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(99, 430);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(15, 14);
+            this.checkBox1.TabIndex = 12;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // tempBox
+            // 
+            this.tempBox.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.tempBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tempBox.Location = new System.Drawing.Point(285, 172);
+            this.tempBox.Name = "tempBox";
+            this.tempBox.Size = new System.Drawing.Size(739, 26);
+            this.tempBox.TabIndex = 13;
+            this.tempBox.TextChanged += new System.EventHandler(this.tempBox_TextChanged);
+            // 
+            // dateBox
+            // 
+            this.dateBox.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.dateBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateBox.Location = new System.Drawing.Point(285, 205);
+            this.dateBox.Name = "dateBox";
+            this.dateBox.Size = new System.Drawing.Size(739, 26);
+            this.dateBox.TabIndex = 14;
+            this.dateBox.TextChanged += new System.EventHandler(this.dateBox_TextChanged);
+            // 
+            // nameBox
+            // 
+            this.nameBox.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.nameBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameBox.Location = new System.Drawing.Point(285, 239);
+            this.nameBox.Name = "nameBox";
+            this.nameBox.Size = new System.Drawing.Size(739, 26);
+            this.nameBox.TabIndex = 15;
+            this.nameBox.TextChanged += new System.EventHandler(this.nameBox_TextChanged);
+            // 
+            // addressBox
+            // 
+            this.addressBox.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.addressBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addressBox.Location = new System.Drawing.Point(285, 271);
+            this.addressBox.Name = "addressBox";
+            this.addressBox.Size = new System.Drawing.Size(739, 26);
+            this.addressBox.TabIndex = 16;
+            this.addressBox.TextChanged += new System.EventHandler(this.addressBox_TextChanged);
+            // 
+            // numberBox
+            // 
+            this.numberBox.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.numberBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numberBox.Location = new System.Drawing.Point(285, 306);
+            this.numberBox.Name = "numberBox";
+            this.numberBox.Size = new System.Drawing.Size(739, 26);
+            this.numberBox.TabIndex = 17;
+            this.numberBox.TextChanged += new System.EventHandler(this.numberBox_TextChanged);
+            // 
+            // emailBox
+            // 
+            this.emailBox.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.emailBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.emailBox.Location = new System.Drawing.Point(285, 341);
+            this.emailBox.Name = "emailBox";
+            this.emailBox.Size = new System.Drawing.Size(739, 26);
+            this.emailBox.TabIndex = 18;
+            this.emailBox.TextChanged += new System.EventHandler(this.emailBox_TextChanged);
+            // 
+            // submit
+            // 
+            this.submit.BackColor = System.Drawing.Color.LimeGreen;
+            this.submit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.submit.ForeColor = System.Drawing.Color.White;
+            this.submit.Location = new System.Drawing.Point(515, 499);
+            this.submit.Name = "submit";
+            this.submit.Size = new System.Drawing.Size(90, 38);
+            this.submit.TabIndex = 19;
+            this.submit.Text = "Submit";
+            this.submit.UseVisualStyleBackColor = false;
+            this.submit.Click += new System.EventHandler(this.submit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1123, 549);
+            this.Controls.Add(this.submit);
+            this.Controls.Add(this.emailBox);
+            this.Controls.Add(this.numberBox);
+            this.Controls.Add(this.addressBox);
+            this.Controls.Add(this.nameBox);
+            this.Controls.Add(this.dateBox);
+            this.Controls.Add(this.tempBox);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.email);
+            this.Controls.Add(this.number);
+            this.Controls.Add(this.address);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.dateTime);
+            this.Controls.Add(this.bodyTemp);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.logo);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Visitor Contact Tracing Form";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.logo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList imageList1;
+        protected System.Windows.Forms.PictureBox logo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label bodyTemp;
+        private System.Windows.Forms.Label dateTime;
+        private System.Windows.Forms.Label name;
+        private System.Windows.Forms.Label address;
+        private System.Windows.Forms.Label number;
+        private System.Windows.Forms.Label email;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox tempBox;
+        private System.Windows.Forms.TextBox dateBox;
+        private System.Windows.Forms.TextBox nameBox;
+        private System.Windows.Forms.TextBox addressBox;
+        private System.Windows.Forms.TextBox numberBox;
+        private System.Windows.Forms.TextBox emailBox;
+        private System.Windows.Forms.Button submit;
+    }
+}
+
---
+++ Abueg_Assignment_06/Abueg_Assignment_06/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Abueg_Assignment_06
+{
+    public partial class Form1 : Form
+    {
+        string temp_ = "";
+        string date_ = "";
+        string name_ = "";
+        string address_ = "";
+        string number_ = "";
+        string email_ = "";
+        string filename = @"C:\Users\prenc\Documents\GitHub\OOP-Assignments\Abueg_Assignment_06\ContactTracingForm.txt";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            logo.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void tempBox_TextChanged(object sender, EventArgs e)
+        {
+            temp_ = tempBox.Text;
+            if (!double.TryParse(temp_, out _) && tempBox.Text != "")
+            {
+                MessageBox.Show("Only input numbers please.");
+                tempBox.Text = "";
+            }
+        }
+
+        private void dateBox_TextChanged(object sender, EventArgs e)
+        {
+            date_ = dateBox.Text;
+        }
+
+        private void nameBox_TextChanged(object sender, EventArgs e)
+        {
+            name_ = nameBox.Text;
+        }
+
+        private void addressBox_TextChanged(object sender, EventArgs e)
+        {
+            address_ = addressBox.Text;
+        }
+
+        private void numberBox_TextChanged(object sender, EventArgs e)
+        {
+            number_ = numberBox.Text;
+            if (!long.TryParse(number_, out _) && numberBox.Text != "")
+            {
+                MessageBox.Show("Only input numbers please.");
+                numberBox.Text = "";
+            }
+        }
+
+        private void emailBox_TextChanged(object sender, EventArgs e)
+        {
+            email_ = emailBox.Text;
+        }
+
+        private void submit_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                if (temp_ != "" && date_ != "" && name_ != "" & address_ != "" && number_ != "" && email_ != "")
+                {
+                    if (!File.Exists(filename))
+                    {
+                        using (StreamWriter sw = File.CreateText(filename))
+                        {
+                            sw.WriteLine("----------------------------------------------------------------------");
+                            sw.WriteLine("Body Temp: {0}", temp_);
+                            sw.WriteLine("Date & Time {0}", date_);
+                            sw.WriteLine("Name: {0}", name_);
+                            sw.WriteLine("Address: {0}", address_);
+                            sw.WriteLine("Contact No.: {0}", number_);
+                            sw.WriteLine("Email: {0}", email_);
+                            sw.WriteLine("----------------------------------------------------------------------");
+                            sw.WriteLine(" ");
+                        }
+                    }
+                    else if (File.Exists(filename))
+                    {
+                        using (StreamWriter sw = File.AppendText(filename))
+                        {
+                            sw.WriteLine("----------------------------------------------------------------------");
+                            sw.WriteLine("Body Temp: {0}", temp_);
+                            sw.WriteLine("Date & Time {0}", date_);
+                            sw.WriteLine("Name: {0}", name_);
+                            sw.WriteLine("Address: {0}", address_);
+                            sw.WriteLine("Contact No.: {0}", number_);
+                            sw.WriteLine("Email: {0}", email_);
+                            sw.WriteLine("----------------------------------------------------------------------");
+                            sw.WriteLine(" ");
+                        }
+                    }
+                    MessageBox.Show("Successfully submitted");
+                    tempBox.Text = "";
+                    dateBox.Text = "";
+                    nameBox.Text = "";
+                    addressBox.Text = "";
+                    numberBox.Text = "";
+                    emailBox.Text = "";
+                    checkBox1.Checked = false;
+                }
+                else
+                {
+                    MessageBox.Show("Incomplete Information");
+                }
+                
+            }
+            else if (!checkBox1.Checked)
+            {
+                MessageBox.Show("Please check the checkbox");
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
