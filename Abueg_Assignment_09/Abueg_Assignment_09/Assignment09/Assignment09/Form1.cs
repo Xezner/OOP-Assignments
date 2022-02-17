307a4c6b40f82d2ca74a142f6c2ab432dc0588dd
Xezner <prencylabueg@gmail.com>
2022-02-17 23:56:44 +0800

    added read of json file and deserializing the json

--- Abueg_Assignment_09/Abueg_Assignment_09/Assignment09/Assignment09/Form1.cs
+++ Abueg_Assignment_09/Abueg_Assignment_09/Assignment09/Assignment09/Form1.cs
@@ -20,7 +20,20 @@ namespace Assignment09
 
         private void load_Click(object sender, EventArgs e)
         {
+            OpenFileDialog fdlg = new OpenFileDialog();
+            fdlg.Title = "C# Corner Open File Dialog";
+            fdlg.InitialDirectory = @"C:\Users\prenc\Documents\GitHub\OOP-Assignments\Abueg_Assignment_09\Abueg_Assignment_09\";
+            fdlg.Filter = "All files (*.*)|*.*|All files (*.*)|*.*";
+            fdlg.FilterIndex = 2;
+            fdlg.RestoreDirectory = true;
+            if (fdlg.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = fdlg.FileName;
+                string jsonResume = System.IO.File.ReadAllText(fileName);
+                NewResume newResume = JsonNet.Deserialize<NewResume>(jsonResume);
 
+                MessageBox.Show($"surname: { newResume.surname} ");
+            }
         }
 
         private void generate_Click(object sender, EventArgs e)
@@ -36,7 +49,7 @@ namespace Assignment09
 
             MessageBox.Show(jsonResume);
 
-            string filePath = @"C:\Users\prenc\Documents\GitHub\OOP-Assignments\Abueg_Assignment_09\Abueg_Assignment_09\" + surnameBox.Text + firstNameBox.Text + ".txt";
+            string filePath = @"C:\Users\prenc\Documents\GitHub\OOP-Assignments\Abueg_Assignment_09\Abueg_Assignment_09\" + surnameBox.Text + firstNameBox.Text + ".json";
             System.IO.File.WriteAllText(filePath, jsonResume);
 
         }
@@ -50,5 +63,21 @@ namespace Assignment09
         public string surname;
         public string firstname;
         public string middlename;
+        public string address;
+        public string contact;
+        public string email;
+        public string hsName;
+        public string hsYears;
+        public string collegeName;
+        public string collegeCourse;
+        public string collegeYears;
+        public string exp;
+        public string skills;
+        public string charRefName1;
+        public string charRefTitle1;
+        public string charRefContact1;
+        public string charRefName2;
+        public string charRefTitle2;
+        public string charRefContact2;
     }
 }
